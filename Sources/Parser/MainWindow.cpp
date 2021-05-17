7788b39425c74bf8568a68ad92b70e99491f86ad
Alex Nem <darkoffalex@gmail.com>
2021-05-17 22:36:41 +0300

    Парсинг, основной функционал

--- Sources/Parser/MainWindow.cpp
+++ Sources/Parser/MainWindow.cpp
@@ -8,15 +8,16 @@
 MainWindow::MainWindow(QWidget *parent) :
         QWidget(parent),
         ui_(new Ui::MainWindow()),
-        replyInitial_(nullptr),
-        replyRegular_(nullptr)
+        httpManager_(nullptr)
 {
     // Инициализация UI
     ui_->setupUi(this);
 
-    // Инициализация объекта для работы с сетью
+    // Инициализировать HTTP
     httpManager_ = new QNetworkAccessManager(this);
-    //connect(httpAccessManager_, &QNetworkAccessManager::finished, this, &MainWindow::httpInitialRequestFinished);
+
+    // Инициализирвоать комбо-бокс с аггентами
+    this->initAgentsCombo();
 }
 
 /**
@@ -27,53 +28,365 @@ MainWindow::~MainWindow()
     // Уничтожение UI
     delete ui_;
 
-    // Уничтожение объекта для работы с сетью
+    // Уничтожение объекта для работы HTTP
     delete httpManager_;
 }
 
-/** S L O T S **/
+/**
+ * \brief Обновить комбо-бокс с выбором агентов
+ */
+void MainWindow::initAgentsCombo()
+{
+    // Список агентов и соответствующие заголовки
+    QMap<QString,QString> agents = {
+            {"Mozilla Firefox","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"},
+            {"Google Chrome","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
+    };
+
+    // Пройтись по списку и обновить комбо-бокс
+    const auto& m = agents.toStdMap();
+    for(const auto& pair : m)
+    {
+        this->ui_->comboBrowser->addItem(pair.first,pair.second);
+    }
+
+    // Выбрать последний вариант
+    this->ui_->comboBrowser->setCurrentIndex(ui_->comboBrowser->count()-1);
+}
 
 /**
- * \brief Обработка события клика по кнопке подключения
+ * \brief Инициировать HTTP запрос, зарегистрировать обработчики событий для чтения ответа
  */
-void MainWindow::on_btnConnect_clicked()
+void MainWindow::makeHttpRequest()
 {
-    // URL запроса
-    QUrl url("https://mbasic.facebook.com/adezhurov/friends");
+    // Куки
+    QByteArray cookie = QByteArray::fromStdString(ui_->editCookie->toPlainText().toStdString());
+
+    // Заголовок агента (для эмуляции бразуера)
+    QByteArray agent = QByteArray::fromStdString(ui_->comboBrowser->currentData().toString().toStdString());
 
     // Создать запрос
-    QNetworkRequest request(url);
+    QNetworkRequest request(httpUrl_);
     request.setRawHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
     request.setRawHeader("Accept-Encoding","utf-8");
     request.setRawHeader("Accept-Language","ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
     request.setRawHeader("Cache-Control","max-age=0");
     request.setRawHeader("Connection","keep-alive");
-    request.setRawHeader("Cookie","datr=KnOeYNz5xBpkpz6ZJ2lKmINO; sb=h3OeYEsORctrHjS8bIJq7MYl; c_user=100012840103209; xs=44%3AODRc3FvypQrxIg%3A2%3A1620996999%3A-1%3A14350%3A%3AAcUWw2he7Va1LtVR8nON-BOieZznMOw_K6RIjz_AJQ; fr=1j1ylPbtEWadU1vb3.AWXvwLm2U6leXV3m2eRgOjRYfzA.BgoECX.IO.AAA.0.0.BgoECX.AWUNN7CcBe8; wd=1920x929; spin=r.1003799531_b.trunk_t.1621115027_s.1_v.2_");
+    request.setRawHeader("Cookie",cookie);
     request.setRawHeader("Host","mbasic.facebook.com");
     request.setRawHeader("TE","Trailers");
     request.setRawHeader("Upgrade-Insecure-Requests","1");
-    request.setRawHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0");
+    request.setRawHeader("User-Agent",agent);
+
+    // Очистить загруженные данные
+    this->downloadedHtml_.clear();
 
     // Выполнить GET запрос
-    this->replyInitial_ = this->httpManager_->get(request);
+    this->httpReply_ = this->httpManager_->get(request);
 
-    // События ответа
-    connect(replyInitial_, &QIODevice::readyRead, this, &MainWindow::onInitialReplyReadyRead);
-    connect(replyInitial_, &QNetworkReply::errorOccurred, this, &MainWindow::onInitialReplyError);
+    // Связать ответ с обработчками событий
+    connect(httpReply_, &QIODevice::readyRead, this, &MainWindow::onReplyReadyRead);
+    connect(httpReply_, &QNetworkReply::errorOccurred, this, &MainWindow::onReplyError);
+    connect(httpReply_, &QNetworkReply::finished, this, &MainWindow::onReplyFinished);
 }
 
-void MainWindow::onInitialReplyReadyRead()
+/**
+ * \brief Обновить индикатор статуса
+ */
+void MainWindow::updateStatusLabel()
 {
-    this->ui_->textBoxCookie->setPlainText(replyInitial_->readAll());
+    // Кол-во друзей
+    QString friendsCounterLabel =
+            "(" + QString::number(friendsAdded_.size()) +
+            "/" + QString::number(friendsTotal_) + " друзей)";
 
-//    QMessageBox msg;
-//    msg.setTextFormat(Qt::TextFormat::PlainText);
-//    msg.setText(replyInitial_->readAll());
-//    msg.show();
-//    msg.exec();
+    // Названия состояний
+    QMap<Status,QString> statusNames = {};
+    statusNames[Status::eStopped] = "Остановлено";
+    statusNames[Status::ePaused] = "Приостановлено";
+    statusNames[Status::eStopped] = "Остановлено";
+
+    // Установить текст
+    this->ui_->labelStatusName->setText(statusNames[this->status_] + " " + friendsCounterLabel);
 }
 
-void MainWindow::onInitialReplyError(QNetworkReply::NetworkError code)
+/**
+ * \brief Получить кол-во друзей из HTML
+ * \return Удалось ли получить
+ */
+bool MainWindow::parseFriendsTotalCount()
 {
-    qDebug() << code;
+    // Регулярные выражения для извлечения кол-ва
+    QRegularExpression rx0(this->firstIteration_ ? R"(<h3 class="ca k">(.+?)<\/h3>)" : R"(<h3 class="bk k">(.+?)<\/h3>)");
+    QRegularExpression rx1("\\((.+?)\\)");
+
+    // Если было совпадение по первому паттерну
+    auto match = rx0.match(this->downloadedHtml_);
+    if(match.hasMatch() && match.capturedTexts().size() > 1)
+    {
+        // Если было совпадение по второму маттерну
+        match = rx1.match(match.captured(1));
+        if(match.hasMatch() && match.capturedTexts().size() > 1)
+        {
+            // Удалось ли конвертировать строку в число
+            bool converted = false;
+            // Попытка конвертировать строку в число
+            auto number = match.captured(1).simplified().replace(" ","").toInt(&converted);
+            // Установить новое значение
+            this->friendsTotal_ = converted ? number : 0;
+            // Статус операции
+            return converted;
+        }
+    }
+
+    // Парсинг не удался
+    return false;
 }
+
+/**
+ * \brief Получить следующий URL из HTML
+ * \return Удалось ли получить
+ */
+bool MainWindow::parseNextUrl()
+{
+    // Регулярное выражение для получения следующего URL
+    QRegularExpression rx("id=\"m_more_friends\"><a href=\"(.+?)\"><span>");
+
+    // Если было совпадение по паттерну
+    auto match = rx.match(this->downloadedHtml_);
+    if(match.hasMatch() && match.capturedTexts().size() > 1)
+    {
+        auto part = match.captured(1);
+        this->httpUrl_ = QUrl("https://mbasic.facebook.com" + part);
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * \brief Получить данные о друзьях из HTML
+ * \return Удалось ли получить
+ */
+bool MainWindow::parseFriends()
+{
+    // Регулярное выражение для получения данных о друзях
+    QRegularExpression rx0(
+            this->firstIteration_ ?
+            "<a class=\"ce\" href=\"(.+?)\">(.+?)<\\/a><div class=\"cf cg\">(.+?)<\\/div>" :
+            "<a class=\"bp\" href=\"(.+?)\">(.+?)<\\/a><div class=\"bq br\">(.+?)<\\/div>");
+
+    // Регулярное выражения для получения извлечения информации о работе
+    QRegularExpression rx1(this->firstIteration_ ?
+                           R"(<span class="by">(.+?)<\/span>)" :
+                           R"(<span class="bs">(.+?)<\/span>)");
+
+    // Успешно ли получены данные
+    bool parsed = false;
+
+    // Выполнить глобальный поиск
+    auto it = rx0.globalMatch(this->downloadedHtml_);
+
+    // Если есть следущий итератор
+    while (it.hasNext())
+    {
+        // Получить совпадение
+        auto match = it.next();
+
+        // Если совпадение действительно было
+        if(match.hasMatch())
+        {
+            // Парсинг успешен
+            parsed = true;
+
+            // Получить необходимые строки
+            auto link = match.captured(1);
+            auto name = match.captured(2);
+            auto info = match.captured(3);
+
+            // Если ссылка на профиль еще не фигурировала
+            if(!friendsAdded_.contains(link))
+            {
+                // Внести ссылку на профиль в set
+                friendsAdded_.insert(link);
+
+                // Привести ссылку в удобоваримый вид
+                link.replace("?fref=fr_tab","");
+                link.replace("&amp;fref=fr_tab","");
+                link = "https://www.facebook.com" + link;
+
+                // Создать в таблице запись
+                auto table = this->ui_->tableWidget;
+                table->insertRow(table->rowCount());
+                table->setItem(table->rowCount()-1,0,new QTableWidgetItem(name));
+                table->setItem(table->rowCount()-1,1,new QTableWidgetItem(link));
+
+                // Если есть информация о работе
+                auto workMatch = rx1.match(info);
+                if(workMatch.hasMatch() && workMatch.capturedTexts().size() > 1)
+                {
+                    table->setItem(table->rowCount()-1,2,new QTableWidgetItem(workMatch.captured(1)));
+                }
+            }
+        }
+    }
+
+    return parsed;
+}
+
+/** S L O T S **/
+
+/**
+ * \brief Обработчка события готовности чтения ответа
+ * \details Данный сигнал может срабатывать НЕСКОЛЬКО раз при обработке ОДНОГО ответа (данные загружаются порциями)
+ */
+void MainWindow::onReplyReadyRead()
+{
+    // Пополнить буфер данными
+    this->downloadedHtml_ += this->httpReply_->readAll();
+}
+
+/**
+ * \brief Обработка события ошибки ответа
+ * \param code Код ошибки
+ */
+void MainWindow::onReplyError(QNetworkReply::NetworkError code)
+{
+    // Сообщение об ошибке
+    QMessageBox msgBox{};
+    msgBox.setText("Ошибка при чтении ответа (код - " + QString::number(code) + ")");
+    msgBox.setWindowTitle("Ошибка соединения");
+    msgBox.setIcon(QMessageBox::Icon::Warning);
+    msgBox.show();
+    msgBox.exec();
+
+    // Сменить статус
+    this->status_ = Status::eStopped;
+    this->updateStatusLabel();
+}
+
+/**
+ * \brief Обработка события завершения обработки ответа
+ * \details Данный сигнал срабатывает когда ответ считается обработанным (единожды за ответ)
+ */
+void MainWindow::onReplyFinished()
+{
+    // Парсинг основных данных
+    bool countParsed = this->parseFriendsTotalCount();
+    bool urlParsed = this->parseNextUrl();
+    bool friendsParsed = this->parseFriends();
+
+    // Продолжать парсинг
+    bool continueParsing = true;
+
+    // Если статус был изменен на "остановлено" или же не были получены какие-то данные
+    if(status_ == Status::eStopped || !(countParsed && urlParsed && friendsParsed))
+    {
+        // Сменить статус
+        this->status_ = Status::eStopped;
+
+        // Интерфейс
+        this->ui_->gbAuth->setEnabled(true);
+        this->ui_->btnParseStart->setEnabled(true);
+        this->ui_->btnParseStop->setEnabled(false);
+        this->ui_->btnParsePause->setEnabled(false);
+        this->ui_->btnFilter->setEnabled(true);
+
+        // Парсинг остановлен
+        continueParsing = false;
+    }
+    // Если статус был изменен на "приостановлено"
+    else if(status_ == Status::ePaused)
+    {
+        // Интерфейс
+        this->ui_->gbAuth->setEnabled(false);
+        this->ui_->btnParseStart->setEnabled(true);
+        this->ui_->btnParseStop->setEnabled(false);
+        this->ui_->btnParsePause->setEnabled(false);
+        this->ui_->btnFilter->setEnabled(true);
+
+        // Парсинг остановлен
+        continueParsing = false;
+    }
+
+    // Обновить информацию о статусе
+    this->updateStatusLabel();
+    // Это более не первая итерация
+    this->firstIteration_ = false;
+    // Очистить HTML
+    this->downloadedHtml_.clear();
+
+    // Если парсинг продолжается
+    if(continueParsing)
+    {
+        // Инициировать новый запрос (следующая страница)
+        this->makeHttpRequest();
+    }
+}
+
+/**
+ * \brief Клик по конопке "пуск"
+ */
+void MainWindow::on_btnParseStart_clicked()
+{
+    // Если отсутствует имя пользователя
+    if(this->ui_->editUsername->text().isEmpty() || this->ui_->editCookie->toPlainText().isEmpty())
+    {
+        // Сообщение об ошибке
+        QMessageBox msgBox{};
+        msgBox.setText("Не введены необходимые данные. Невозможно начать процесс.");
+        msgBox.setWindowTitle("Ошибка");
+        msgBox.setIcon(QMessageBox::Icon::Warning);
+        msgBox.show();
+        msgBox.exec();
+        return;
+    }
+
+    // Интерфейс
+    this->ui_->gbAuth->setEnabled(false);
+    this->ui_->btnParseStart->setEnabled(false);
+    this->ui_->btnParseStop->setEnabled(true);
+    this->ui_->btnParsePause->setEnabled(true);
+    this->ui_->btnFilter->setEnabled(false);
+
+    // Если последнее состояние - "остановлено"
+    if(this->status_ == Status::eStopped)
+    {
+        // Значит это первая итерация парсинга
+        this->firstIteration_ = true;
+        // Очистить таблицу друзей
+        this->ui_->tableWidget->setRowCount(0);
+        // Очистить set друзей
+        this->friendsAdded_.clear();
+        // Обнулить кол-во
+        this->friendsTotal_ = 0;
+        // Сформировать начальный URL
+        this->httpUrl_ = QUrl("https://mbasic.facebook.com/" + this->ui_->editUsername->text() + "/friends");
+    }
+
+    // Сменить статус
+    this->status_ = Status::eParsing;
+    this->updateStatusLabel();
+
+    // Выполнить первый запрос
+    this->makeHttpRequest();
+}
+
+/**
+ * \brief Клик по конопке "стоп"
+ */
+void MainWindow::on_btnParsePause_clicked()
+{
+    this->ui_->btnParsePause->setEnabled(false);
+    this->status_ = Status::ePaused;
+}
+
+/**
+ * \brief Клик по конопке "пауза"
+ */
+void MainWindow::on_btnParseStop_clicked()
+{
+    this->ui_->btnParseStop->setEnabled(false);
+    this->status_ = Status::eStopped;
+}
+
--- Sources/Parser/MainWindow.h
+++ Sources/Parser/MainWindow.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <QFrame>
+#include <QString>
 #include <QMessageBox>
 #include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
 #include <QtNetwork/QNetworkReply>
+#include <QRegularExpression>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -18,6 +21,20 @@ Q_OBJECT
 
 public:
     /**
+     * \brief Состояния приложения
+     */
+    enum class Status
+    {
+        // Остановлено / не запущено
+        eStopped = 0,
+        // Приостановлено
+        ePaused = 1,
+        // В процессе
+        eParsing = 2
+    };
+
+public:
+    /**
      * \brief Конструктор основного окна
      * \param parent Указатель на родительский виджет
      */
@@ -29,28 +46,94 @@ public:
     ~MainWindow() override;
 
 private slots:
-    /**
-     * \brief Обработка события клика по кнопке подключения
-     */
-    void on_btnConnect_clicked();
 
     /**
-     * \brief Обработка события чтения ответа на первый запрос (инициализации)
+     * \brief Обработчка события готовности чтения ответа
+     * \details Данный сигнал может срабатывать НЕСКОЛЬКО раз при обработке ОДНОГО ответа (данные загружаются порциями)
      */
-    void onInitialReplyReadyRead();
+    void onReplyReadyRead();
 
     /**
-     * \brief Ошибка получения ответа на запрос инициализации
+     * \brief Обработка события ошибки ответа
+     * \param code Код ошибки
      */
-    void onInitialReplyError(QNetworkReply::NetworkError code);
+    void onReplyError(QNetworkReply::NetworkError code);
+
+    /**
+     * \brief Обработка события завершения обработки ответа
+     * \details Данный сигнал срабатывает когда ответ считается обработанным (единожды за ответ)
+     */
+    void onReplyFinished();
+
+    /**
+     * \brief Клик по конопке "пуск"
+     */
+    void on_btnParseStart_clicked();
+
+    /**
+     * \brief Клик по конопке "стоп"
+     */
+    void on_btnParsePause_clicked();
+
+    /**
+     * \brief Клик по конопке "пауза"
+     */
+    void on_btnParseStop_clicked();
 
 private:
     /// Указатель на UI объект
     Ui::MainWindow* ui_;
-    /// Объект для работы HTTP запросами
+
+    /// Объект для работы с HTTP запросами
     QNetworkAccessManager* httpManager_;
-    /// Ответ на первый запрос (инициализации)
-    QNetworkReply* replyInitial_;
-    /// Ответ на запрос парсинга (используется многократно)
-    QNetworkReply* replyRegular_;
+    /// Текущий URL для запросов
+    QUrl httpUrl_ = QUrl("");
+    /// Указатель на объект HTTP ответа
+    QNetworkReply* httpReply_ = nullptr;
+
+    /// Содержимое последнего ответа (HTML)
+    QString downloadedHtml_ = "";
+    /// Общее кол-вл друзей (значение обновляется после первого парсинга)
+    quint32 friendsTotal_ = 0;
+    /// Сет для избежания добавления уже добавленных
+    QSet<QString> friendsAdded_;
+
+    /// Статус
+    Status status_ = Status::eStopped;
+    /// Первая итерация
+    bool firstIteration_ = true;
+
+
+    /**
+     * \brief Обновить комбо-бокс с выбором агентов
+     */
+    void initAgentsCombo();
+
+    /**
+     * \brief Инициировать HTTP запрос, зарегистрировать обработчики событий для чтения ответа
+     */
+    void makeHttpRequest();
+
+    /**
+     * \brief Обновить индикатор статуса
+     */
+    void updateStatusLabel();
+
+    /**
+     * \brief Получить кол-во друзей из HTML
+     * \return Удалось ли получить
+     */
+    bool parseFriendsTotalCount();
+
+    /**
+     * \brief Получить следующий URL из HTML
+     * \return Удалось ли получить
+     */
+    bool parseNextUrl();
+
+    /**
+     * \brief Получить данные о друзьях из HTML
+     * \return Удалось ли получить
+     */
+    bool parseFriends();
 };
